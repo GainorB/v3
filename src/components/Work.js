3ea0aee86b758301530512a618fbb5bc7b4f656a
GainorB <gainorbostwick@gmail.com>
2018-09-13 23:34:15 -0400

    fix ids

--- src/components/Work.js
+++ src/components/Work.js
@@ -68,8 +68,8 @@ class Work extends Component {
   };
 
   renderProjects = projects => {
-    const output = projects.map((p, idx) => (
-      <Link to={{ pathname: `/case-study/${idx}/${p.name}`, state: { project: p } }} key={key()}>
+    const output = projects.map(p => (
+      <Link to={{ pathname: `/case-study/${p.id}/${p.name}`, state: { project: p } }} key={key()}>
         <ProjectGrid>
           <img src={p.image} alt={p.name} />
           <div className="overlay">
